62ca0967cf2e79e7de62913871a7619c2be6aa8f
Veaga <jompawars@gmail.com>
2021-11-28 17:24:59 +0100

    Use Path.Combine for macos compatibility

--- RabbitMQ Test Service/Handlers/FileService.cs
+++ RabbitMQ Test Service/Handlers/FileService.cs
@@ -23,56 +23,46 @@ namespace RabbitMQ_Test_Service
         {
             WorkFolders = new Dictionary<FolderTypeEnum, string>();
             var Workspace = configuration["Workspace"];
-            Workspace ??= Environment.CurrentDirectory + "\\Workspace\\";
+            Workspace ??= Path.Combine(Environment.CurrentDirectory, "Workspace");
             WorkFolders.Add(FolderTypeEnum.Workspace, Workspace);
-            WorkFolders.Add(FolderTypeEnum.Incomming, Workspace + "\\Inc\\");
-            WorkFolders.Add(FolderTypeEnum.Processed, Workspace + "\\Processed\\");
-            WorkFolders.Add(FolderTypeEnum.Error, Workspace + "\\Err\\");
+            WorkFolders.Add(FolderTypeEnum.Incomming, Path.Combine(Workspace, "Inc\\"));
+            WorkFolders.Add(FolderTypeEnum.Processed, Path.Combine(Workspace, "Processed\\"));
+            WorkFolders.Add(FolderTypeEnum.Error, Path.Combine(Workspace, "Err\\"));
             FoldersCheck();
         }
 
         public void FoldersCheck()
         {
             foreach (KeyValuePair<FolderTypeEnum, string> entry in WorkFolders)
-            {
-                var pathArr = entry.Value.Split('\\');
-                for (int i = 1; i <= pathArr.Length; i++)
-                {
-                    var path = string.Join("\\", pathArr.Take(i));
-                    if (path.Length == 2) continue;
-                    if (!Directory.Exists(path))
-                        Directory.CreateDirectory(path);
-                }
-            }
+                Directory.CreateDirectory(entry.Value);
         }
 
         internal void WriteToIncomming(byte[] body)
         {
-            File.WriteAllBytes($"{WorkFolders[FolderTypeEnum.Incomming]}\\{GenerateFilename("simp")}", body);
+            File.WriteAllBytes(Path.Combine(WorkFolders[FolderTypeEnum.Incomming], GenerateFilename("simp")), body);
         }
 
         private string GenerateFilename(string suffix)
         {
-            if (suffix[0] == '.')
-                suffix.Remove(0);
+            if (suffix[0] == '.') suffix.Remove(0);
             return $"Received-{DateTime.Now.Ticks}.{suffix}";
         }
 
         internal List<SimpleMessage> ScrapeIncommingSimpleMessages()
         {
-            var files = Directory.GetFiles(WorkFolders[FolderTypeEnum.Incomming]);
+            var filePaths = Directory.GetFiles(WorkFolders[FolderTypeEnum.Incomming]);
             List<SimpleMessage> simpleMessages = new();
-            foreach (string path in files)
+            foreach (string filePath in filePaths)
             {
                 try
                 {
-                    var body = File.ReadAllText(path);
+                    var body = File.ReadAllText(filePath);
                     simpleMessages.Add(Newtonsoft.Json.JsonConvert.DeserializeObject<SimpleMessage>(body));
-                    MoveToProcessed(path);
+                    MoveToProcessed(filePath);
                 }
                 catch
                 {
-                    MoveToError(path);
+                    MoveToError(filePath);
                 }
             }
 
@@ -84,19 +74,24 @@ namespace RabbitMQ_Test_Service
             return path.Split('\\').Last();
         }
 
+        private static string GetSuffix(string path)
+        {
+            return path.Split('.').Last();
+        }
+
         private void MoveToError(string path)
         {
-            File.Move(path, WorkFolders[FolderTypeEnum.Error] + GetFileName(path) + "simperr");
+            File.Move(path, Path.Combine(WorkFolders[FolderTypeEnum.Error], GetFileName(path), "simperr"));
         }
 
         private void MoveToProcessed(string path)
         {
-            File.Move(path, WorkFolders[FolderTypeEnum.Processed] + GetFileName(path));
+            File.Move(path, Path.Combine(WorkFolders[FolderTypeEnum.Processed], GetFileName(path)));
         }
 
         internal void SaveToError(SimpleMessage msg)
         {
-            File.WriteAllText($"{WorkFolders[FolderTypeEnum.Error]}\\{GenerateFilename(".simperr")}", Newtonsoft.Json.JsonConvert.SerializeObject(msg));
+            File.WriteAllText(Path.Combine(WorkFolders[FolderTypeEnum.Error], GenerateFilename(".simperr")), Newtonsoft.Json.JsonConvert.SerializeObject(msg));
         }
     }
 }
